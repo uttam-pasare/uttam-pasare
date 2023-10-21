2809822ff378b07c882e5ae8ccbc0a89d5822161
uttam-pasare <uttam.r.pasare@gmail.com>
2023-10-21 11:58:59 +0530

    Explained Java 8 functions with simple apply function and chaining of functions

---
+++ readme.md
@@ -0,0 +1,5 @@
+This project is intended for learning how Java 8 functions works.
+
+Following examples are elaborated in this project
+1. How Function<T,R> works - Simple example on capitalize name of the employee with function which takes one argument as input and returns the output
+2. Chaining of functions - Exmmple around chaining two functions together and getting desired output.
---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import model.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee().setName("John").setId(100).setGender("Male").setSalary(25000.00));
+        employees.add(new Employee().setName("Alex").setId(100).setGender("Male").setSalary(24000.00));
+        employees.add(new Employee().setName("Merry").setId(100).setGender("Female").setSalary(23000.00));
+        employees.add(new Employee().setName("Sam").setId(100).setGender("Male").setSalary(26000.00));
+        employees.add(new Employee().setName("Angela").setId(100).setGender("Female").setSalary(27000.00));
+
+        // 1. Function which takes one input and returns output
+        // Capitalize names and print
+        Function<String,String> capitalizeFunction = String::toUpperCase;
+        employees.stream().map(Employee::getName).forEach(name -> System.out.println(capitalizeFunction.apply(name)));
+
+        // 2. Chaining of functions
+        // Capitalize then substring the length and print
+        Function<String,String> substringFunction = name -> name.substring(2);
+        employees.stream().map(Employee::getName).forEach(name -> System.out.println(capitalizeFunction.andThen(substringFunction).apply(name)));
+    }
+}
